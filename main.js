cdc477ee1d2220323c395c1e73b2411309b3feb0
marton bognar <martonbognar@sch.bme.hu>
2015-11-07 17:33:14 +0100

    Remove jquery for now

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).on("swipe", function() {
-  alert(window.location.href.indexOf("newcomers"));
-  var s = window.location.href;
-  if (s.indexOf("newcomers") > -1) {
-    window.location.assign('/');
-  } else {
-    window.location.assign('/newcomers');
-  }
-});
